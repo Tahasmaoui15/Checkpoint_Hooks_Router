2da28c3639e5da3dabc32248966307fbbc728787
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-31 16:05:48 +0100

    Added more movies to Movies.js.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,44 +1,86 @@
 const Movies = [
     {
-        title: "A Beautiful Min",
+        title: "A Beautiful Mind",
         description: "The film is driven by the agonizing conflict between Nash's mathematical brilliance and the paranoid schizophrenia which almost destroys both his career and his marriage to Alicia Larde",
         posterURL: "https://www.movieposters.com/cdn/shop/products/97b11eee3160ba9d71ba657d79e29a38_90dec42c-668b-4662-a0a6-95801e432dae_480x.progressive.jpg?v=1573590358",
-        rating: "4/5"
+        rating: "4"
     },
 
     {
         title: "Braveheart",
         description: "William Wallace is the medieval Scottish patriot who is spurred into revolt against the English when the love of his life is slaughtered.",
         posterURL: "https://www.movieposters.com/cdn/shop/products/7eda7d058cc637169abb122d4b8f1228_23b02d7c-29ae-4f14-b1ec-f05d380300eb_480x.progressive.jpg?v=1573652592",
-        rating: ""
+        rating: "2.5"
     },
 
     {
         title: "Jurassic Park",
         description: "The story is about Jurassic Park, a nature reserve on a remote South American island owned by billionaire businessman John Hammond. Hammond and his team of dedicated scientists have populated the park with genetically engineered dinosaurs.",
         posterURL: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/4940c5878babf3dc5d2ca567b7558178_9e62fc4c-4116-48e5-a4f5-3a99c73ae7b1_500x749.jpg?v=1573651499",
-        rating: "4/5"
+        rating: "3"
     },
 
     {
         title: "Pulp Fiction",
         description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
         posterURL: "https://www.movieposters.com/cdn/shop/products/pulpfiction.2436_480x.progressive.jpg?v=1620048742",
-        rating: "5/5"
+        rating: "5"
     },
 
     {
         title: "Home Alone",
         description: "An eight-year-old troublemaker, mistakenly left home alone, must defend his home against a pair of burglars on Christmas Eve.",
         posterURL: "https://www.movieposters.com/cdn/shop/products/dd43b3a44eacfd9ff0a3d97b311d17d2_b4766a06-e1c1-421d-acdb-3adae5c2c645_480x.progressive.jpg?v=1573652567",
-        rating: "3.5/5"
+        rating: "3.5"
     },
 
     {
         title: "The Godfather",
         description: "The story of the powerful Italian-American crime family, the Corleones. Filled with themes of loyalty, betrayal, and power.",
         posterURL: "https://www.movieposters.com/cdn/shop/products/b5282f72126e4919911509e034a61f66_6ce2486d-e0da-4b7a-9148-722cdde610b8_480x.progressive.jpg?v=1573616025",
-        rating: "5/5"
+        rating: "5"
+    },
+
+    {
+        title: "Inglorious B#$%@rs",
+        description: "It is the first year of Germany's occupation of France. Allied officer Lt. Aldo Raine assembles a team of Jewish soldiers to commit violent acts of retribution against the Nazis.",
+        posterURL: "https://www.movieposters.com/cdn/shop/products/inglourious-basterds-style4_37d01d94-318d-4840-a290-6fc5aa7b8072_480x.progressive.jpg?v=1615396029",
+        rating: "4.5",
+    },
+
+    {
+        title: "The Dark Knight",
+        description: "With the help of allies Lt. Jim Gordon and DA Harvey Dent, Batman has been able to keep a tight lid on crime in Gotham City until the appearance of the chaotic Joker.",
+        posterURL: "https://www.movieposters.com/cdn/shop/files/dark_knight_ver5_480x.progressive.jpg?v=1692579075",
+        rating: "4.5",
+    },
+
+    {
+        title: "The Persuit Of Happyness",
+        description: "Life is a struggle for single father Chris Gardner. Evicted from their apartment, he and his young son find themselves alone with no place to go.",
+        posterURL: "https://www.movieposters.com/cdn/shop/products/bd0f30ecb2cb52e660d9f186f1e2a0cf_86cbdbd2-70f5-4827-96f8-2773689ad27e_480x.progressive.jpg?v=1573652345",
+        rating: "4",
+    },
+
+    {
+        title: "The Matrix",
+        description: "Neo believes that Morpheus, an elusive figure considered to be the most dangerous man alive, can answer his question -- What is the Matrix?",
+        posterURL: "https://www.movieposters.com/cdn/shop/files/scan_f48bfe00-c291-441c-8bcb-63abbe283fed_480x.progressive.jpg?v=1688653768",
+        rating: "3.5",
+    },
+
+    {
+        title: "Scarface",
+        description: "After getting a green card in exchange for assassinating a Cuban government official, Tony Montana stakes a claim on the drug trade in Miami.",
+        posterURL: "https://www.movieposters.com/cdn/shop/products/2300c0258501297acf11acf055321423_480x.progressive.jpg?v=1573572641",
+        rating: "4",
+    },
+
+    {
+        title: "Catch Me If You Can",
+        description: "Frank Abagnale, a 17 years old master of deception become the most successful forger and bank robber whic hmakes him the top priority of FBI agent Carl Hanratty.",
+        posterURL: "https://www.movieposters.com/cdn/shop/products/89bf74b3f600774f9d9072b36e27d4b5_07564e66-b867-4073-b8eb-a54d1337f096_480x.progressive.jpg?v=1573587254",
+        rating: "4",
     },
 ];
 
